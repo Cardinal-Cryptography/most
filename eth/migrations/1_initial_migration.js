42b7ccf458897e81c4e3b35a7a41c30df18a6f1a
filip <fbielejec@gmail.com>
2023-09-04 17:03:47 +0200

    clean target

--- eth/migrations/1_initial_migration.js
+++ eth/migrations/1_initial_migration.js
@@ -5,15 +5,14 @@ const TruffleConfig = require('../truffle-config.js');
 module.exports = function(deployer, network, accounts) {
     const config = TruffleConfig.networks[network];
     const opts = { gas: 6e6, from: accounts[0] };
-    
+
     if (process.env.ACCOUNT_PASSWORD) {
         const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
         console.log('Unlocking account: ' + accounts[0]);
-        web3.personal.unlockAccount(accounts[0], process.env.ACCOUNT_PASSWORD, 36000);
+
+        web3.eth.personal.unlockAccount(accounts[0], process.env.ACCOUNT_PASSWORD, 86400);
+        deployer.deploy(Migrations, opts);        
     } else {
         throw new Error('provide ACCOUNT_PASSWORD ev var');
     }
-    
-
-    deployer.deploy(Migrations, opts);
 };
