bb4700cc4159ca0cbdbe6f45c1cbcf2672d5c26a
filip <fbielejec@gmail.com>
2023-09-06 10:32:01 +0200

    error impls

--- relayer/src/eth_contract_listener.rs
+++ relayer/src/eth_contract_listener.rs
@@ -2,22 +2,26 @@ use crate::config::Config;
 use ethers::{
     contract::abigen,
     core::types::Address,
+    prelude::ContractError,
     providers::{Provider, ProviderError, StreamExt, Ws},
 };
 use eyre::Result;
 use std::sync::Arc;
 use thiserror::Error;
 
-// #[derive(Debug, Error)]
-// #[error(transparent)]
-// #[non_exhaustive]
-// pub enum EthListenerError {
-//     #[error("provider error")]
-//     Provider(#[from] ProviderError),
+#[derive(Debug, Error)]
+#[error(transparent)]
+#[non_exhaustive]
+pub enum EthListenerError {
+    #[error("provider error")]
+    Provider(#[from] ProviderError),
 
-//     #[error("error when parsing ethereum address")]
-//     FromHex(#[from] rustc_hex::FromHexError),
-// }
+    #[error("error when parsing ethereum address")]
+    FromHex(#[from] rustc_hex::FromHexError),
+
+    #[error("contract error")]
+    Contract(#[from] ContractError<Provider<Ws>>),
+}
 
 abigen!(
     Flipper,
@@ -26,7 +30,7 @@ abigen!(
     ]"#,
 );
 
-pub async fn run(config: Arc<Config>) -> Result<()> {
+pub async fn run(config: Arc<Config>) -> Result<(), EthListenerError> {
     let Config {
         eth_node_wss_url,
         eth_contract_address,
@@ -41,5 +45,10 @@ pub async fn run(config: Arc<Config>) -> Result<()> {
 
     let events = contract.event::<FlipFilter>().from_block(16232696);
     let mut stream = events.stream().await?.take(1);
+
+    while let Some(Ok(f)) = stream.next().await {
+        println!("Flipper event: {f:?}");
+    }
+
     Ok(())
 }
