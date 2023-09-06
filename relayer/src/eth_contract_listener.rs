85bc4e59b4a90ad61ef98618c505dc48b46ad526
filip <fbielejec@gmail.com>
2023-09-06 13:04:45 +0200

    chunk past event

--- relayer/src/eth_contract_listener.rs
+++ relayer/src/eth_contract_listener.rs
@@ -2,8 +2,9 @@ use crate::config::Config;
 use ethers::{
     contract::abigen,
     core::types::Address,
-    prelude::ContractError,
+    prelude::{Contract, ContractError},
     providers::{Provider, ProviderError, StreamExt, Ws},
+    types::{BlockNumber, Filter, H160, H256},
 };
 // use eyre::Result;
 use std::sync::Arc;
@@ -30,6 +31,7 @@ abigen!(
     ]"#,
 );
 
+// 1337..BlockNumber::Latest
 pub async fn run(config: Arc<Config>) -> Result<(), EthListenerError> {
     let Config {
         eth_node_wss_url,
@@ -38,18 +40,70 @@ pub async fn run(config: Arc<Config>) -> Result<(), EthListenerError> {
         ..
     } = &*config;
 
-    let provider = Provider::<Ws>::connect(eth_node_wss_url).await?;
+    let provider = connect(eth_node_wss_url).await?;
     let client = Arc::new(provider);
-    let address: Address = eth_contract_address.parse()?;
 
-    let contract: Flipper<Provider<Ws>> = Flipper::new(address, client);
+    // let token_topics = vec![H256::from(eth_contract_address.parse::<H160>()?)];
 
-    let events = contract.event::<FlipFilter>().from_block(*eth_from_block);
-    let mut stream = events.stream().await?.take(1);
+    let address = eth_contract_address.parse::<Address>()?;
+    let contract = Flipper::new(address, client);
+    let events = contract.events();
 
-    while let Some(Ok(f)) = stream.next().await {
-        println!("Flipper event: {f:?}");
+    // replay past events
+    // TODO : in chunks
+    let past_events = events
+        .from_block(0)
+        .to_block(BlockNumber::Latest)
+        .query()
+        .await?;
+    println!("{:?}", past_events);
+    println!("{}", past_events.len());
+
+    // subscribe to new events
+
+    let events = contract.events().from_block(0);
+    let mut stream = events.stream().await?.with_meta();
+    while let Some(Ok((event, meta))) = stream.next().await {
+        println!("{event:?}, {meta:?}");
     }
 
     Ok(())
+}
+
+async fn connect(url: &str) -> Result<Provider<Ws>, EthListenerError> {
+    Ok(Provider::<Ws>::connect(url).await?)
+}
+
+fn chunks(from: i32, to: i32, step: i32) -> Vec<(i32, i32)> {
+    let mut intervals = Vec::new();
+    let mut current = from;
+
+    while current < to {
+        let next = current + step;
+        if next > to {
+            intervals.push((current, to));
+        } else {
+            intervals.push((current, next));
+        }
+        current = next;
+    }
+
+    intervals
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_chunks() {
+        let from = 0;
+        let to = 21;
+        let step = 5;
+        let intervals = chunks(from, to, step);
+        assert_eq!(
+            vec![(0, 5), (5, 10), (10, 15), (15, 20), (20, 21)],
+            intervals
+        );
+    }
 }
