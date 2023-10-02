d12c742e7bfdb44d6d41c14eb01bf58c165cc3ad
Paweł Obrok <pawel.obrok@cardinals.cc>
2023-10-02 18:19:33 +0200

    Deploy eth contracts to bridgenet on master push

--- eth/migrations/2_deploy_contracts.js
+++ eth/migrations/2_deploy_contracts.js
@@ -1,27 +1,15 @@
-const Web3 = require('web3');
-const fs = require('fs');
+const fs = require('node:fs');
 
-const Flipper = artifacts.require("Flipper");
+const Flipper = artifacts.require('Flipper');
 const TruffleConfig = require('../truffle-config.js');
 
-module.exports = async function(deployer, network, accounts) {
-    const config = TruffleConfig.networks[network];
-    const opts = { gas: 1e6, from: accounts[0] };
-    if (process.env.ACCOUNT_PASSWORD) {
-        const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
-        console.log('Unlocking account: ' + accounts[0]);
-        await web3.eth.personal.unlockAccount(accounts[0], process.env.ACCOUNT_PASSWORD, 86400);
+module.exports = async function (deployer, network, accounts) {
+	const options = {gas: 1e6, from: accounts[0]};
+	await deployer.deploy(Flipper, options);
+	const instance = await Flipper.deployed();
 
-        await deployer.deploy(Flipper, opts);
-        let instance = await Flipper.deployed();
-       
-        let addresses = {
-            'flipper': instance.address
-        };
-        fs.writeFileSync('addresses.json', JSON.stringify(addresses));
-
-    } else {
-        throw new Error('provide ACCOUNT_PASSWORD ev var');
-    }
-
+	const addresses = {
+		flipper: instance.address,
+	};
+	fs.writeFileSync('addresses.json', JSON.stringify(addresses));
 };
