0a27ef06c1d2e16e2adb2ba2ac0dd5b561956c54
filip <fbielejec@gmail.com>
2023-10-04 13:37:31 +0200

    fmt

--- eth/contracts/Flipper.sol
+++ eth/contracts/Flipper.sol
@@ -1,25 +1,24 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Flipper {
-  bool public flipValue;
-  bool public flopValue;  
+    bool public flipValue;
+    bool public flopValue;
 
-  event Flip(bool value);
-  event Flop(bool value);  
+    event Flip(bool value);
+    event Flop(bool value);
 
-  constructor() public {
-    flipValue = false;
-    flopValue = false;    
-  }
+    constructor() public {
+        flipValue = false;
+        flopValue = false;
+    }
 
-  function flip() public {
-    flipValue = !flipValue;
-    emit Flip(flipValue);
-  }
+    function flip() public {
+        flipValue = !flipValue;
+        emit Flip(flipValue);
+    }
 
-  function flop() public {
-    flopValue = !flopValue;
-    emit Flop(flopValue);
-  }
-  
+    function flop() public {
+        flopValue = !flopValue;
+        emit Flop(flopValue);
+    }
 }
