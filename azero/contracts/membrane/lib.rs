3affb5a92ad766fbb28981e17c0f7fb32319882c
filip <fbielejec@gmail.com>
2023-10-04 22:14:41 +0200

    methods impl

--- azero/contracts/membrane/lib.rs
+++ azero/contracts/membrane/lib.rs
@@ -14,7 +14,11 @@ mod membrane {
 
     #[ink(event)]
     #[derive(Debug)]
-    pub struct Fu {}
+    pub struct CrosschainTransferRequest {}
+
+    #[ink(event)]
+    #[derive(Debug)]
+    pub struct RequestProcessed {}
 
     #[derive(Debug, Encode, Decode, Clone, Copy, PartialEq, Eq)]
     #[cfg_attr(feature = "std", derive(scale_info::TypeInfo, StorageLayout))]
@@ -37,10 +41,28 @@ mod membrane {
 
     pub type Event = <Membrane as ContractEventBase>::Type;
 
+    #[derive(Debug, PartialEq, Eq, Encode, Decode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum MembraneError {
+        NotGuardian,
+    }
+
     impl Membrane {
         #[ink(constructor)]
         pub fn new(guardians: Vec<AccountId>, signature_threshold: u128) -> Self {
-            todo!()
+            let mut guardians_set = Mapping::new();
+            guardians.into_iter().for_each(|account| {
+                guardians_set.insert(account, &());
+            });
+
+            Self {
+                request_nonce: 0,
+                signature_threshold,
+                pending_requests: Mapping::new(),
+                request_signatures: Mapping::new(),
+                processed_requests: Mapping::new(),
+                guardians: guardians_set,
+            }
         }
 
         #[ink(message)]
@@ -59,6 +81,14 @@ mod membrane {
         // pub fn get(&self) -> (bool, bool) {
         //     (self.flip, self.flop)
         // }
+
+        fn is_guardian(&self, account: AccountId) -> Result<(), MembraneError> {
+            if self.guardians.contains(account) {
+                Ok(())
+            } else {
+                Err(MembraneError::NotGuardian)
+            }
+        }
 
         fn emit_event<EE>(emitter: EE, event: Event)
         where
