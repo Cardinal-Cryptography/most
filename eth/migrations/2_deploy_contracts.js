581c433a3f5b800cc8656372ad60fe779c1ad68a
Filip Bielejec <fbielejec@gmail.com>
2023-10-13 11:43:44 +0200

    Update eth/migrations/2_deploy_contracts.js
    Co-authored-by: Krzysztof Ziobro <86822080+krzysztofziobro@users.noreply.github.com>

--- eth/migrations/2_deploy_contracts.js
+++ eth/migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const Membrane = artifacts.require("Membrane");
 const TruffleConfig = require("../truffle-config.js");
 
 module.exports = async function (deployer, network, accounts) {
-  const options = { gas: 1e6, from: accounts[0] };
+  const options = { gas: 2e6, from: accounts[0] };
   await deployer.deploy(Membrane, [accounts[0]], 1, options);
   const instance = await Membrane.deployed();
 
