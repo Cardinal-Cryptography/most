894d9472d4eb4801cd026b484f9a51c5fa7681b6
Filip Bielejec <fbielejec@gmail.com>
2023-10-17 18:01:45 +0200

    A0-3301: mint burn (#10)
    * mint/burn
    
    * wuut
    
    * modify deploy
    
    * cleanup
    
    * rename

--- azero/contracts/psp22/lib.rs
+++ azero/contracts/psp22/lib.rs
@@ -1,11 +1,10 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 #[ink::contract]
-mod psp22 {
-    use ink::{
-        codegen::EmitEvent, prelude::vec::Vec, reflect::ContractEventBase, storage::Mapping,
-    };
-    use psp22_traits::{PSP22Error, PSP22};
+pub mod token {
+    use ink::prelude::{string::String, vec::Vec};
+    use psp22::{PSP22Data, PSP22Error, PSP22Event, PSP22Metadata, PSP22};
+    use psp22_traits::{Burnable, Mintable};
 
     #[ink(event)]
     pub struct Approval {
@@ -13,188 +12,172 @@ mod psp22 {
         owner: AccountId,
         #[ink(topic)]
         spender: AccountId,
-        amount: Balance,
+        amount: u128,
     }
 
     #[ink(event)]
     pub struct Transfer {
         #[ink(topic)]
-        from: AccountId,
+        from: Option<AccountId>,
         #[ink(topic)]
-        to: AccountId,
-        value: Balance,
+        to: Option<AccountId>,
+        value: u128,
     }
 
     #[ink(storage)]
-    #[derive(Default)]
     pub struct Token {
-        total_supply: Balance,
-        balances: Mapping<AccountId, Balance>,
-        allowances: Mapping<(AccountId, AccountId), Balance>,
+        data: PSP22Data,
+        name: Option<String>,
+        symbol: Option<String>,
+        decimals: u8,
     }
-
-    pub type Event = <Token as ContractEventBase>::Type;
 
     impl Token {
         #[ink(constructor)]
-        pub fn new(total_supply: Balance) -> Self {
-            let mut balances = Mapping::default();
-            let caller = Self::env().caller();
-            balances.insert(caller, &total_supply);
+        pub fn new(
+            total_supply: u128,
+            name: Option<String>,
+            symbol: Option<String>,
+            decimals: u8,
+        ) -> Self {
             Self {
-                total_supply,
-                balances,
-                allowances: Default::default(),
+                data: PSP22Data::new(total_supply, Self::env().caller()),
+                name,
+                symbol,
+                decimals,
             }
         }
 
-        fn _approve_from_to(
-            &mut self,
-            owner: AccountId,
-            spender: AccountId,
-            amount: Balance,
-        ) -> Result<(), PSP22Error> {
-            self.allowances.insert((&owner, &spender), &amount);
-
-            Self::emit_event(
-                self.env(),
-                Event::Approval(Approval {
-                    owner,
-                    spender,
-                    amount,
-                }),
-            );
-
-            Ok(())
-        }
-
-        fn _transfer_from_to(
-            &mut self,
-            from: &AccountId,
-            to: &AccountId,
-            value: Balance,
-            _data: Vec<u8>,
-        ) -> Result<(), PSP22Error> {
-            let from_balance = self.balance_of(*from);
-            if from_balance < value {
-                return Err(PSP22Error::InsufficientBalance);
+        fn emit_events(&self, events: Vec<PSP22Event>) {
+            for event in events {
+                match event {
+                    PSP22Event::Transfer { from, to, value } => {
+                        self.env().emit_event(Transfer { from, to, value })
+                    }
+                    PSP22Event::Approval {
+                        owner,
+                        spender,
+                        amount,
+                    } => self.env().emit_event(Approval {
+                        owner,
+                        spender,
+                        amount,
+                    }),
+                }
             }
+        }
+    }
 
-            // NOTE: this should never underflow / overflow as the u128::MAX is orders of magnitude larger
-            // than typical amount of tokens in circluation
-            self.balances.insert(from, &(from_balance - value));
-            let to_balance = self.balance_of(*to);
-            self.balances.insert(to, &(to_balance + value));
-
-            Self::emit_event(
-                self.env(),
-                Event::Transfer(Transfer {
-                    from: *from,
-                    to: *to,
-                    value,
-                }),
-            );
-
-            Ok(())
+    impl PSP22Metadata for Token {
+        #[ink(message)]
+        fn token_name(&self) -> Option<String> {
+            self.name.clone()
         }
 
-        fn emit_event<EE>(emitter: EE, event: Event)
-        where
-            EE: EmitEvent<Self>,
-        {
-            emitter.emit_event(event);
+        #[ink(message)]
+        fn token_symbol(&self) -> Option<String> {
+            self.symbol.clone()
+        }
+
+        #[ink(message)]
+        fn token_decimals(&self) -> u8 {
+            self.decimals
         }
     }
 
     impl PSP22 for Token {
-        /// Returns the total token supply.
         #[ink(message)]
-        fn total_supply(&self) -> Balance {
-            self.total_supply
+        fn total_supply(&self) -> u128 {
+            self.data.total_supply()
         }
 
-        /// Returns the account balance for the specified `owner`.
         #[ink(message)]
-        fn balance_of(&self, owner: AccountId) -> Balance {
-            self.balances.get(owner).unwrap_or_default()
+        fn balance_of(&self, owner: AccountId) -> u128 {
+            self.data.balance_of(owner)
         }
 
-        /// Returns the amount which `spender` is allowed to withdraw on behalf of the `owner` account.
         #[ink(message)]
-        fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
-            self.allowances.get((owner, spender)).unwrap_or_default()
+        fn allowance(&self, owner: AccountId, spender: AccountId) -> u128 {
+            self.data.allowance(owner, spender)
         }
 
-        /// Allows `spender` to withdraw from the caller's account multiple times, up to the `value` amount.
-        #[ink(message)]
-        fn approve(&mut self, spender: AccountId, amount: Balance) -> Result<(), PSP22Error> {
-            let owner = self.env().caller();
-            self._approve_from_to(owner, spender, amount)
-        }
-
-        /// Increase `spender`'s allowance to withdraw from the caller's account by the `by` amount.
-        #[ink(message)]
-        fn increase_allowance(
-            &mut self,
-            spender: AccountId,
-            by: Balance,
-        ) -> Result<(), PSP22Error> {
-            let owner = Self::env().caller();
-            self._approve_from_to(owner, spender, self.allowance(owner, spender) + by)
-        }
-
-        /// Decrease `spender`'s allowance to withdraw from the caller's account by the `by` amount.
-        #[ink(message)]
-        fn decrease_allowance(
-            &mut self,
-            spender: AccountId,
-            by: Balance,
-        ) -> Result<(), PSP22Error> {
-            let owner = Self::env().caller();
-            let allowance = self.allowance(owner, spender);
-
-            if allowance < by {
-                return Err(PSP22Error::InsufficientAllowance);
-            }
-
-            self._approve_from_to(owner, spender, allowance - by)
-        }
-
-        /// Transfers `value` amount of tokens from the caller's account to account `to`.
         #[ink(message)]
         fn transfer(
             &mut self,
             to: AccountId,
-            value: Balance,
-            data: Vec<u8>,
+            value: u128,
+            _data: Vec<u8>,
         ) -> Result<(), PSP22Error> {
-            let from = self.env().caller();
-            self._transfer_from_to(&from, &to, value, data)
+            let events = self.data.transfer(self.env().caller(), to, value)?;
+            self.emit_events(events);
+            Ok(())
         }
 
-        /// Transfers `value` amount of tokens on the behalf of `from` to the account `to`.
-        /// Caller need to be pre-approved
         #[ink(message)]
         fn transfer_from(
             &mut self,
             from: AccountId,
             to: AccountId,
-            value: Balance,
-            data: Vec<u8>,
+            value: u128,
+            _data: Vec<u8>,
         ) -> Result<(), PSP22Error> {
-            let caller = self.env().caller();
-            let allowance = self.allowance(from, caller);
+            let events = self
+                .data
+                .transfer_from(self.env().caller(), from, to, value)?;
+            self.emit_events(events);
+            Ok(())
+        }
 
-            if allowance < value {
-                return Err(PSP22Error::InsufficientAllowance);
-            }
+        #[ink(message)]
+        fn approve(&mut self, spender: AccountId, value: u128) -> Result<(), PSP22Error> {
+            let events = self.data.approve(self.env().caller(), spender, value)?;
+            self.emit_events(events);
+            Ok(())
+        }
 
-            self._transfer_from_to(&from, &to, value, data)?;
+        #[ink(message)]
+        fn increase_allowance(
+            &mut self,
+            spender: AccountId,
+            delta_value: u128,
+        ) -> Result<(), PSP22Error> {
+            let events = self
+                .data
+                .increase_allowance(self.env().caller(), spender, delta_value)?;
+            self.emit_events(events);
+            Ok(())
+        }
 
-            // NOTE: can you spot a potential storage optimization here?
-            self.allowances
-                .insert((&from, &caller), &(allowance - value));
+        #[ink(message)]
+        fn decrease_allowance(
+            &mut self,
+            spender: AccountId,
+            delta_value: u128,
+        ) -> Result<(), PSP22Error> {
+            let events = self
+                .data
+                .decrease_allowance(self.env().caller(), spender, delta_value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+    }
 
+    // TODO : access control (roles)
+    impl Mintable for Token {
+        #[ink(message)]
+        fn mint(&mut self, to: AccountId, value: u128) -> Result<(), PSP22Error> {
+            let events = self.data.mint(to, value)?;
+            self.emit_events(events);
+            Ok(())
+        }
+    }
+
+    // TODO : access control (roles)
+    impl Burnable for Token {
+        #[ink(message)]
+        fn burn(&mut self, from: AccountId, value: u128) -> Result<(), PSP22Error> {
+            let events = self.data.burn(from, value)?;
+            self.emit_events(events);
             Ok(())
         }
     }
