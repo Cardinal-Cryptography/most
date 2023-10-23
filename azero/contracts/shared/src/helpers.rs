8c76b10cad75c54a01dbfe3addec78a40aed4d62
filip <fbielejec@gmail.com>
2023-10-23 16:20:44 +0200

    helpers

---
+++ azero/contracts/shared/src/helpers.rs
@@ -0,0 +1,9 @@
+use ink::prelude::vec::Vec;
+
+pub fn concat_u8_arrays(arrays: Vec<&[u8]>) -> Vec<u8> {
+    let mut result = Vec::new();
+    for array in arrays {
+        result.extend_from_slice(array);
+    }
+    result
+}
