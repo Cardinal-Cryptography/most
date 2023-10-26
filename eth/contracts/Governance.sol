4a3236ab740f47e14e4a26719511095b4e7ed6b6
filip <fbielejec@gmail.com>
2023-10-26 14:52:43 +0200

    wip Governance.sol

---
+++ eth/contracts/Governance.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+/* import "@openzeppelin/contracts/token/ERC20/IERC20.sol"; */
+
+contract Governance {
+
+  address public owner;
+  uint256 public quorum;
+  mapping(address => bool) members;
+  mapping(uint256 => Proposal) pendingProposals;
+  
+  struct Proposal {
+    address destination;
+    bytes payload;      
+    uint256 signatureCount;
+    mapping(address => bool) signatures;
+  }
+
+  modifier _onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+    
+  constructor(uint256 _quorum) {
+    owner = msg.sender;
+    quorum = _quorum;
+  }
+    
+  // set owner
+    
+}
