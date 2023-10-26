f2ea6cf32ad846e296f2e2a937323367e022067d
filip <fbielejec@gmail.com>
2023-10-26 16:36:57 +0200

    getter/setters

--- eth/contracts/Governance.sol
+++ eth/contracts/Governance.sol
@@ -21,7 +21,7 @@ contract Governance {
 
   event Vote(address by, uint256 id);
 
-  event ProposalExecuted(address by, uint256 id);
+  event ProposalExecuted(address by, uint256 id, bytes data);
 
   modifier _onlyOwner() {
     require(msg.sender == owner);
@@ -29,7 +29,7 @@ contract Governance {
   }
 
   modifier _onlyMember() {
-    require(isMember(msg.sender), "Can only be called by a comittee member");
+    require(isMember(msg.sender), "NotMember");
     _;
   }
 
@@ -54,14 +54,57 @@ contract Governance {
 
   function vote(uint256 id) external _onlyMember {
 
+    Proposal storage proposal = pendingProposals[id];
 
+    require(proposal.signatures[msg.sender] == false, "ProposalAlreadySigned");
 
+    proposal.signatureCount += 1;
+    proposal.signatures[msg.sender] = true;
+
+    emit Vote(msg.sender, id);
+  }
+
+  function executeProposal(uint256 id) external {
+
+    Proposal storage proposal = pendingProposals[id];
+
+    require(hasQuorum(id), "NoQuorum");
+
+    (bool success, bytes memory result) = proposal.destination.call(proposal.payload);
+
+    require(success, "ExecuteProposalFailed");
+
+    delete pendingProposals[id];
+
+    emit ProposalExecuted(msg.sender, id, result);
+  }
+
+  function hasQuorum(uint256 id) public view returns (bool) {
+    return pendingProposals[id].signatureCount >= quorum;
   }
 
   function isMember(address _address) public view returns (bool) {
     return members[_address];
   }
 
-  // TODO set owner
+  function getSignatureCount(uint256 id) external view returns (uint256) {
+    return pendingProposals[id].signatureCount;
+  }
+
+  function setQuorum(uint256 _quorum) external _onlyOwner {
+    quorum = _quorum;
+  }
+
+  function setOwner(address _owner) external _onlyOwner {
+    owner = _owner;
+  }
+
+  function addMember(address member) external _onlyOwner {
+    members[member] = true;
+  }
+
+  function removeMember(address member) external _onlyOwner {
+    members[member] = false;
+  }
 
 }
