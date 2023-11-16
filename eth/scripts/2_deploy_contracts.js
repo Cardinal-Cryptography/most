798c74b573552c69cabf914896a05958ac5c856f
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2023-11-16 12:14:19 +0100

    Init

--- eth/scripts/2_deploy_contracts.js
+++ eth/scripts/2_deploy_contracts.js
@@ -2,11 +2,21 @@ const fs = require("node:fs");
 const hre = require("hardhat");
 
 async function main() {
+  const QUORUM = 5;
+
   const accounts = await hre.ethers.getSigners();
+  const guardians = accounts.slice(1, 9);
+
+  // Governance
+  const Governance = await hre.ethers.getContractFactory("Governance");
+  const governance = await Governance.deploy(guardians, QUORUM);
+  const governanceAddress = await governance.getAddress();
+
+  console.log("Governance deployed to:",  governanceAddress);
 
   // Membrane
   const Membrane = await hre.ethers.getContractFactory("Membrane");
-  const membrane = await Membrane.deploy(accounts.slice(1, 9), 5);
+  const membrane = await Membrane.deploy(guardians, QUORUM);
   const membraneAddress = await membrane.getAddress();
 
   console.log("Membrane deployed to:",  membraneAddress);
@@ -19,11 +29,16 @@ async function main() {
   console.log("Wrapped Ether deployed to:",  wrappedEtherAddress);
 
   const addresses = {
+    governance: governanceAddress,
     membrane: membraneAddress,
     wrappedEther: wrappedEtherAddress,
   };
 
   fs.writeFileSync("addresses.json", JSON.stringify(addresses));
+
+  //await governance.setOwner(governance);
+  //await membrane.setOwner(governance);
+  //await membrane.addPair(hre.ethers.zeroPadValue(wrappedEtherAddress, 32), )
 }
 
 main().catch((error) => {
