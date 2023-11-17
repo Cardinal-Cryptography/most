9275764aac8f1a8850b0d189e9de307ad86f193a
Krzysztof Ziobro <jerzym27@protonmail.com>
2023-11-17 18:12:48 +0100

    Remove unnecessary test

--- relayer/src/helpers.rs
+++ relayer/src/helpers.rs
@@ -5,20 +5,3 @@ pub fn concat_u8_arrays(arrays: Vec<&[u8]>) -> Vec<u8> {
     }
     result
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_chunks() {
-        let from = 0;
-        let to = 21;
-        let step = 5;
-        let intervals = chunks(from, to, step);
-        assert_eq!(
-            vec![(0, 5), (5, 10), (10, 15), (15, 20), (20, 21)],
-            intervals
-        );
-    }
-}
