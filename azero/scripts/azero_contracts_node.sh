44d704f23c42db154a2af19f68af258945a6ce02
Krzysztof Ziobro <jerzym27@protonmail.com>
2024-01-03 14:26:13 +0100

    Rollback docker-compose -> docker compose in contracts node script

--- azero/scripts/azero_contracts_node.sh
+++ azero/scripts/azero_contracts_node.sh
@@ -2,4 +2,4 @@
 
 # This script is used to start the bridge node with stdout redirected to stderr and replaced localhost address
 # These changes are needed for the node to be compatible with ink_e2e tests
-docker compose -f ../../../devnet-azero/devnet-azero-compose.yml up | sed 's/0\.0\.0\.0/127.0.0.1/' 1>&2
+docker-compose -f ../../../devnet-azero/devnet-azero-compose.yml up | sed 's/0\.0\.0\.0/127.0.0.1/' 1>&2
