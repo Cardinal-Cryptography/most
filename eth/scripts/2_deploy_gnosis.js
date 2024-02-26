1e731f30d4602c72d18c32d8f342963a19f86fbb
filip <fbielejec@gmail.com>
2024-02-26 18:34:59 +0100

    add ore dpeloyments to create SDK

--- eth/scripts/2_deploy_gnosis.js
+++ eth/scripts/2_deploy_gnosis.js
@@ -1,6 +1,6 @@
-const { ContractNetworksConfig, SafeFactory } = require('@safe-global/protocol-kit');
+const Safe = require('@safe-global/protocol-kit').default;
+const { SafeFactory, EthersAdapter } = require('@safe-global/protocol-kit');
 const { network, ethers } = require("hardhat");
-const { EthersAdapter } = require('@safe-global/protocol-kit');
 
 async function main() {
 
@@ -17,49 +17,54 @@ async function main() {
         const GnosisSafeProxyFactory = await ethers.getContractFactory("GnosisSafeProxyFactory");
         console.log("Deploying GnosisSafeProxyFactory...");
         let gnosisSafeProxyFactory = await GnosisSafeProxyFactory.deploy();
-        await gnosisSafeProxyFactory.deploymentTransaction().wait(1);
         console.log("GnosisSafeProxyFactory deployed to:", gnosisSafeProxyFactory.target);
 
-        // const GnosisSafe = await ethers.getContractFactory("GnosisSafe");
-        // console.log("Deploying GnosisSafe...");
-        // const gnosisSafe = await GnosisSafe.deploy();
-        // console.log("GnosisSafe deployed to:", gnosisSafe.target);
+        const Singleton = await ethers.getContractFactory("Singleton");
+        console.log("Deploying Singleton...");
+        let singleton = await Singleton.deploy();
+        console.log("Singleton deployed to:", singleton.target);
 
-        // const MultiSend = await ethers.getContractFactory("MultiSend");
-        // console.log("Deploying MultiSend...");
-        // const multiSend = await MultiSend.deploy();
-        // console.log("MultiSend deployed to:", multiSend.target);
+        const GnosisSafeProxy = await ethers.getContractFactory("GnosisSafeProxy");
+        console.log("Deploying GnosisSafeProxy...");
+        let gnosisSafeProxy = await GnosisSafeProxy.deploy(singleton.target);
+        console.log("GnosisSafeProxy deployed to:", gnosisSafeProxy.target);
+
+        const GnosisSafe = await ethers.getContractFactory("GnosisSafe");
+        console.log("Deploying GnosisSafe...");
+        const gnosisSafe = await GnosisSafe.deploy();
+        console.log("GnosisSafe deployed to:", gnosisSafe.target);
+
+        const MultiSend = await ethers.getContractFactory("MultiSend");
+        console.log("Deploying MultiSend...");
+        const multiSend = await MultiSend.deploy();
+        console.log("MultiSend deployed to:", multiSend.target);
 
         const chainId = await ethAdapter.getChainId()
 
-        // console.log("@@@ address :", await gnosisSafeProxyFactory.getAddress());
-
-        // const contractNetworks = ContractNetworksConfig.new();
-
-        // await sleep(10000);
-
-        const isContractDeployed = await ethAdapter.isContractDeployed(await gnosisSafeProxyFactory.getAddress());
-        console.log("@@@ deployed? :", isContractDeployed);
+        // await some confirmations, else it gets finicky
+        await gnosisSafeProxyFactory.deploymentTransaction().wait(1);
+        await gnosisSafeProxy.deploymentTransaction().wait(1);
+        await singleton.deploymentTransaction().wait(1);
 
         const contractNetworks  = {
             [chainId]: {
-
-                // multiSendAddress: multiSend.target,
-                // // multiSendAbi: multiSendAbi,
-
-                // safeMasterCopyAddress: gnosisSafe.target,
-                // safeMasterCopyAbi: safeAbi,
-
-                safeProxyFactoryAddress: await gnosisSafeProxyFactory.getAddress(),
-                // safeProxyFactoryAbi: gnosisSafeProxyFactory.interface,
-
+                multiSendAddress: multiSend.target,
+                safeSingletonAddress:  singleton.target,
+                safeProxy:  gnosisSafeProxy.target,
+                safeProxyFactoryAddress:  gnosisSafeProxyFactory.target,
+                safeMasterCopyAddress: gnosisSafe.target,
             }
 
         }
 
-        // console.log("@@@ contractNetworks", contractNetworks);
+        console.log("Gnosis Safe contracts", contractNetworks);
 
         const safeFactory = await SafeFactory.create({ ethAdapter, contractNetworks });
+
+        // create new Safe
+
+        const safeSdk = await Safe.create({ ethAdapter, safeAddress: gnosisSafe.target, contractNetworks })
+
 
         break;
         // TODO: for other networks augment hardhat config with the addresses of the already deployed gnosis contracts
