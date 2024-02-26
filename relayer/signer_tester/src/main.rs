32a376b86345c5d70b60cf7724d3e512d0496f17
Paweł Obrok <pawel.obrok@cardinals.cc>
2024-02-26 22:00:43 +0100

    Switch to tokio in signer

--- relayer/signer_tester/src/main.rs
+++ relayer/signer_tester/src/main.rs
@@ -9,15 +9,21 @@ struct Arguments {
     port: u32,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args = Arguments::parse();
-    let client =
-        signer_client::Client::new(args.cid, args.port).expect("Failed to connect to signer");
+    let mut client = signer_client::Client::new(args.cid, args.port)
+        .await
+        .expect("Failed to connect to signer");
 
     let azero_account_id = client
         .azero_account_id()
+        .await
         .expect("Failed to get Azero account ID");
-    let eth_address = client.eth_address().expect("Failed to get ETH address");
+    let eth_address = client
+        .eth_address()
+        .await
+        .expect("Failed to get ETH address");
 
     println!("Azero account ID: {:?}", azero_account_id);
     println!("ETH address: {:?}", eth_address);
