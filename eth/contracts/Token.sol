1a7d0bf6d3da7566dc74c125f8070800bf13e7eb
JanKuczma <jan.kuczma@cardinals.cc>
2024-03-05 12:01:12 +0100

    Allow custom decimals in Token.sol

--- eth/contracts/Token.sol
+++ eth/contracts/Token.sol
@@ -7,11 +7,19 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
+    uint8 private _decimals;
+    
     constructor(
         uint256 _totalSupply,
+        uint8 decimals_,
         string memory _name,
         string memory _symbol
     ) ERC20(_name, _symbol) {
+        _decimals = decimals_;
         _mint(msg.sender, _totalSupply);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
     }
 }
