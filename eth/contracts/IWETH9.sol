358b35425e240a2c8b6eb3a36d95f7da93447563
JanKuczma <jan.kuczma@cardinals.cc>
2024-03-07 17:38:44 +0100

    interface for IWETH9

---
+++ eth/contracts/IWETH9.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.2;
+
+/// @title Interface for WETH9
+interface IWETH9 {
+    /// @notice Deposit ether to get wrapped ether
+    function deposit() external payable;
+
+    /// @notice Withdraw wrapped ether to get ether
+    function withdraw(uint256) external;
+}
