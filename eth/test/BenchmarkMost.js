ef1d4b8109034fc66e67a3d15fdeec2c4e0bbe3e
Krzysztof Ziobro <jerzym27@protonmail.com>
2024-06-03 18:20:52 +0200

    Add USDT bench

--- eth/test/BenchmarkMost.js
+++ eth/test/BenchmarkMost.js
@@ -21,6 +21,10 @@ describe("MostBenchmark", function () {
     const weth = await Weth.deploy();
     const wethAddress = await weth.getAddress();
 
+    const USDT = await hre.ethers.getContractFactory("TetherToken");
+    const usdt = await USDT.deploy("10000", "Tether USD", "USDT", "6");
+    const usdtAddress = await usdt.getAddress();
+
     const Most = await hre.ethers.getContractFactory("Most");
     const most = await upgrades.deployProxy(
       Most,
@@ -33,13 +37,20 @@ describe("MostBenchmark", function () {
     const mostAddress = await most.getAddress();
 
     // Easy way to get a "random" bytes32 value
-    const azeroContract = getRandomAlephAccount(42);
-    const azeroContract2 = getRandomAlephAccount(43);
+    const azeroWrappedToken = getRandomAlephAccount(42);
+    const azeroWrappedWeth = getRandomAlephAccount(43);
+    const azeroWrappedUsdt = getRandomAlephAccount(44);
+
     const tokenAddressBytes32 = addressToBytes32(tokenAddress);
+    const wethAddressBytes32 = addressToBytes32(wethAddress);
+    const usdtAddressBytes32 = addressToBytes32(usdtAddress);
 
     // Add pair of linked contracts
-    await most.addPair(tokenAddressBytes32, azeroContract, { from: accounts[0] });
-    await most.addPair(addressToBytes32(wethAddress), azeroContract2, {
+    await most.addPair(tokenAddressBytes32, azeroWrappedToken, { from: accounts[0] });
+    await most.addPair(wethAddressBytes32, azeroWrappedWeth, {
+      from: accounts[0],
+    });
+    await most.addPair(usdtAddressBytes32, azeroWrappedUsdt, {
       from: accounts[0],
     });
     await most.unpause();
@@ -79,9 +90,29 @@ describe("MostBenchmark", function () {
       from: accounts[0],
     });
 
+    // Send usdt
+    await usdt.approve(mostAddress, amount, {
+      from: accounts[0],
+    });
+
+    const gasEstimateSendUsdt = await most.sendRequest.estimateGas(
+      usdtAddressBytes32,
+      amount,
+      azeroAccount,
+      { from: accounts[0] },
+    );
+
+    console.log("Gas estimate for sendRequest USDT: ", Number(gasEstimateSendUsdt));
+
+    await most.sendRequest(usdtAddressBytes32, amount, azeroAccount, {
+      gas: gasEstimateSendUsdt,
+      from: accounts[0],
+    });
+
+    // Send native ETH
     const gasEstimateSendNative = await most.sendRequestNative.estimateGas(
       azeroAccount,
-      { from: accounts[0], value: 2*amount },
+      { from: accounts[0], value: 2 * amount },
     );
 
     console.log(
@@ -92,7 +123,7 @@ describe("MostBenchmark", function () {
     await most.sendRequestNative(azeroAccount, {
       gas: gasEstimateSendNative,
       from: accounts[0],
-      value: 2*amount,
+      value: 2 * amount,
     });
 
     // Gas estimate for bridgeReceive
@@ -117,7 +148,19 @@ describe("MostBenchmark", function () {
       guardianKeys,
       committeeSize,
       committeeId,
-      addressToBytes32(wethAddress),
+      usdtAddressBytes32,
+      amount,
+      ethAccount,
+      ++nonce,
+      "USDT",
+    );
+
+    await benchmarkReceiveRequest(
+      most,
+      guardianKeys,
+      committeeSize,
+      committeeId,
+      wethAddressBytes32,
       amount,
       ethAccount,
       ++nonce,
@@ -134,7 +177,7 @@ describe("MostBenchmark", function () {
       zeroAddress,
       amount,
       ethAccount,
-      nonce,
+      ++nonce,
       "weth - unwrap",
     );
   });
