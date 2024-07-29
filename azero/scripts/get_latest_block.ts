74f565e1912ee0eb1d57953cdb237987df141f75
Mikolaj Gasior <g@f0x.pl>
2024-07-29 15:23:26 +0200

    Add getting latest block

---
+++ azero/scripts/get_latest_block.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+
+import { ApiPromise, WsProvider, Keyring } from "@polkadot/api";
+import { import_env } from "./utils";
+import "dotenv/config";
+import "@polkadot/api-augment";
+
+const envFile = process.env.AZERO_ENV;
+
+async function main(): Promise<void> {
+  if (!envFile) {
+    throw new Error("Please provide an env file");
+  }
+
+  const config = await import_env(envFile);
+  const { ws_node } = config;
+  const wsProvider = new WsProvider(ws_node);
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const latestBlockHash = await api.rpc.chain.getFinalizedHead()
+  const latestBlock = await api.rpc.chain.getBlock(latestBlockHash)
+  const block_latest = {
+    number: latestBlock.block.header.number
+  }
+  fs.writeFileSync("block_latest.json", JSON.stringify(block_latest))
+  await api.disconnect();
+  console.log("Done");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ eth/scripts/get_latest_block.js
@@ -0,0 +1,15 @@
+const fs = require("node:fs");
+const { network, ethers } = require("hardhat");
+
+async function main() {
+  const latestBlock = await ethers.provider.getBlock("latest");
+  const block_latest = {
+    number: latestBlock.number
+  }
+  fs.writeFileSync("block_latest.json", JSON.stringify(block_latest))
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+})
