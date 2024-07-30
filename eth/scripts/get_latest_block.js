a7b1e5d5f76563440521a011f0198dbb6c09d6d7
Mikolaj Gasior <g@f0x.pl>
2024-07-30 11:49:33 +0200

    Satisfy prettier

--- eth/scripts/get_latest_block.js
+++ eth/scripts/get_latest_block.js
@@ -4,12 +4,12 @@ const { network, ethers } = require("hardhat");
 async function main() {
   const latestBlock = await ethers.provider.getBlock("latest");
   const block_latest = {
-    number: latestBlock.number
-  }
-  fs.writeFileSync("block_latest.json", JSON.stringify(block_latest))
+    number: latestBlock.number,
+  };
+  fs.writeFileSync("block_latest.json", JSON.stringify(block_latest));
 }
 
 main().catch((error) => {
   console.error(error);
   process.exit(1);
-})
+});
