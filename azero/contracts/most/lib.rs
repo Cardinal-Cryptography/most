d92236f7d9010e8d4a7736149c3a818e393916a2
Jan Koscisz <koscis.j@gmail.com>
2024-09-24 17:52:03 +0200

    fix

--- azero/contracts/most/lib.rs
+++ azero/contracts/most/lib.rs
@@ -449,6 +449,8 @@ pub mod most {
                 .checked_sub(amount_to_bridge)
                 .ok_or(MostError::ValueTransferredLowerThanAmount)?;
 
+            let amount = amount_to_bridge.saturating_mul(RATIO);
+
             let wrapped_azero_address = self.wazero.get().ok_or(MostError::WrappedAzeroNotSet)?;
             let wrapped_azero_address_bytes: [u8; 32] = *wrapped_azero_address.as_ref();
             let mut wrapped_azero: contract_ref!(WrappedAZERO) = wrapped_azero_address.into();
@@ -456,15 +458,13 @@ pub mod most {
             wrapped_azero
                 .call_mut()
                 .deposit()
-                .transferred_value(amount_to_bridge)
+                .transferred_value(amount)
                 .invoke()?;
 
             let dest_token_address = self
                 .supported_pairs
                 .get(wrapped_azero_address_bytes)
                 .ok_or(MostError::UnsupportedPair)?;
-
-            let amount = amount_to_bridge.saturating_mul(RATIO);
 
             self._send_request(
                 wrapped_azero_address,
